eb0541e851be667c8571b7dc86c535ab804e8359
Sachin <92576557+Sachim91@users.noreply.github.com>
2023-02-12 04:18:55 +0000

    local,seession storage

---
+++ web developmentt/sharpner/dom/local.js
@@ -0,0 +1,11 @@
+localStorage.setItem("name","bob");
+localStorage.removeItem("name");
+
+
+sessionStorage.setItem('name','jhn')
+sessionStorage.removeItem('name');
+
+
+document.cookie = 'name=kyle';expires = '+new Date(9999.02,2)'
+
+console.log(document.cookie); 
